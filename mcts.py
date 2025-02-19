046a32b0dd0039942120d6e98de23741ba614f42
Emin ACAR <117777931+NeoAcar@users.noreply.github.com>
2025-02-19 19:12:31 +0300

    Add files via upload
    bir gün temel ayakkabıcıya gitmiş bir ayakkabı gimiş ayakkabı temelin ayağını sıkmış temel satıcıya ha bu ayakkabu ayağumı sıkayur da demiş satıcı 2 hafta sikar demiş temel de eyi ozaman 2 hafta giymem habuni demiş

---
+++ mcts.py
@@ -0,0 +1,151 @@
+import math
+import utils as f
+import numpy as np
+import torch
+from env_final import Kita
+from model import ResNet
+import copy
+global max_depth 
+max_depth = set()
+
+class Node:
+    def __init__(self, args:'dict', state:'Kita', depth:'int'=0, parent:'Node | None'=None, action:'int|None'=None, prior:'float|None'=None, policy:'np.ndarray|None'=None):
+        self.args = args
+        self.state = state
+        self.parent = parent
+        self.prior = prior
+        self.policy = policy
+        self.action = action
+        self.depth = depth
+        
+        
+
+        self.Q = 0.
+        self.N = 0
+        self.children:list[Node] = []
+
+    def is_terminal(self) -> bool:
+        return self.state.check_gameover()
+
+    def is_leaf(self) -> bool:
+        return len(self.children) == 0
+    
+    def ucb(self, child: 'Node') -> float:
+        c_base = self.args["c_base"]
+        c_init = self.args["c_init"]
+        t = self.args["t"]
+        return ((child.Q + 1e-6) / (child.N + 1e-12)) +(math.log((1 + self.N + c_base) / c_base) + c_init) * (child.prior** (1/t)) * (math.sqrt(self.N) / (1 + child.N))
+
+
+    def best_child(self) -> 'Node':
+        child_ucbs = [self.ucb(child) for child in self.children]
+        return self.children[np.argmax(child_ucbs)]
+    
+    @torch.no_grad()
+    def value_policy_batch(self, states: 'list[Kita]', model: 'ResNet') -> tuple[np.ndarray, np.ndarray]:
+        
+        model.eval()
+        inputs = torch.stack([f.prepare_input(state) for state in states]).to(self.args['device'])
+        values, policies = model(inputs)
+        values = values.cpu().numpy()
+        policies = torch.softmax(policies, dim=1).cpu().numpy()
+        
+        
+        valid_policies = f.parallel_valid_policy(policies, states) 
+        
+        return values, valid_policies
+
+    def expand(self, model: 'ResNet') -> None:
+        top_actions = self.args['top_actions']
+        new_states = []
+        actions = []
+        probs = []
+        top_actions_probs = sorted(enumerate(self.policy), key=lambda x: x[1], reverse=True)[:top_actions]
+        for action, prob in top_actions_probs:
+            if prob > 0:
+                new_state = copy.deepcopy(self.state)
+                move = f.a0_to_move(action)
+                new_state.move(move)
+                new_states.append(new_state)
+                actions.append(action)
+                probs.append(prob)
+                
+                
+        values,policies = self.value_policy_batch(new_states, model)
+        
+        max_depth.add(self.depth+1)
+        for i, state in enumerate(new_states):
+            if state.check_gameover():
+                values[i] = -1000
+            child = Node(self.args, new_states[i], self.depth+1, self, actions[i], probs[i], policies[i])
+            self.children.append(child)
+            child.backpropagation(-values[i])
+     
+
+    def backpropagation(self, rollout_value: float) -> None:
+        self.N += 1
+
+        self.Q += rollout_value
+        node = self.parent
+        while node is not None:
+            rollout_value = -rollout_value
+            node.N += 1
+            node.Q += rollout_value
+            node = node.parent
+
+class MCTS:
+    def __init__(self, args: 'dict', model: 'ResNet') -> None:
+        self.args = args
+        self.model = model.to(args['device'])
+
+
+
+
+    @torch.no_grad()
+    def value_policy(self, state: 'Kita') -> tuple[float, np.ndarray]:
+       
+        self.model.eval()
+        value , policy = self.model(f.prepare_input(state).unsqueeze(0).to(self.args['device']))
+        
+        
+        #value = value.cpu().item()
+        policy = torch.softmax(policy.squeeze(0), dim=0).cpu().numpy()
+       
+        valid_policy = f.valid_policy(policy, state)
+        return value, valid_policy
+
+
+
+
+    def _simulate(self, root: 'Node') -> None:
+        current_node = root
+        while not current_node.is_leaf():
+            current_node = current_node.best_child()
+
+        if current_node.is_terminal():
+            value = -1000 if current_node.state.turn == 1 else 0
+            current_node.backpropagation(value)
+        else:
+            current_node.expand(self.model)
+
+
+
+    def search(self, state: 'Kita') -> np.ndarray:
+        max_depth.clear()
+        root_state = copy.deepcopy(state)
+        value, policy = self.value_policy(root_state)
+     
+        policy = (1 - self.args['dirichlet_epsilon']) * policy + self.args['dirichlet_epsilon'] * np.random.dirichlet(
+            [self.args['dirichlet_alpha']] * self.args['action_space'])
+        policy = f.valid_policy(policy, root_state)
+        root = Node(self.args, root_state)
+        root.policy = policy
+
+        for _ in range(self.args["num_simulation"]):
+            self._simulate(root)
+        print("max_depth", max(max_depth ))
+        mcts_action_probs = np.zeros(self.args['action_space'])
+        for child in root.children:
+            mcts_action_probs[child.action] = child.N
+        return mcts_action_probs / np.sum(mcts_action_probs)
+    
---
+++ utils.py
@@ -0,0 +1,123 @@
+import torch
+import numpy as np
+from env_final import Kita
+
+def board_to_matrix(env: Kita) -> np.ndarray:
+    matrix = np.zeros((10, 4, 7), dtype=np.float32)
+    
+    matrix[0, *env.pieces[env.turn][0]] = 1
+    matrix[1, *env.pieces[env.turn][1]] = 1
+    matrix[1, *env.pieces[env.turn][2]] = 1
+    
+    matrix[2, *env.pieces[-env.turn][0]] = 1
+    matrix[3, *env.pieces[-env.turn][1]] = 1
+    matrix[3, *env.pieces[-env.turn][2]] = 1
+
+    matrix[4, :, :] = (env.turn + 1)//2
+    matrix[5, :, :] = float(env.move_counter)/100
+    
+    if env.move_counter > 1:
+        matrix[6, *env.last_moves[env.turn][0]] = 1
+        matrix[6, *env.last_moves[env.turn][1]] = 1
+    
+    if env.move_counter > 0:
+        matrix[7, *env.last_moves[-env.turn][0]] = 1
+        matrix[7, *env.last_moves[-env.turn][1]] = 1
+
+    matrix[8] = env.tile_values
+
+    matrix[9] = [
+            [1, 1,     1,     1,  1,     1,     1],
+            [1,  0,  0,  1,  0,  0,  1],
+            [1,   0,  0,  1,  0,  0,  1],
+            [1, 1, 1, 1,  1,     1,     1]
+        ]
+    
+    return matrix
+
+def move_to_a0(move: str) -> int:
+    print(move)
+    start_col = ord(move[0]) - 97
+    start_row = int(move[1]) - 1
+    end_col = ord(move[2]) - 97
+    end_row = int(move[3]) - 1
+    start_idx = start_col + start_row * 7
+
+    col_diff = end_col - start_col
+    row_diff = end_row - start_row
+
+    if col_diff == 0:
+        move_type_index = row_diff + 2 if row_diff > 0 else row_diff+3
+    elif row_diff == 0:
+        move_type_index = col_diff + 8 if col_diff > 0 else col_diff+9
+    else:
+        if abs(row_diff)-abs(col_diff) == 0:
+            move_type_index = (row_diff+2*col_diff+3)//2+12
+        elif abs(row_diff) == 2:
+            move_type_index = (row_diff+col_diff+3)//2+16
+        elif abs(col_diff) == 2:
+            move_type_index = (row_diff+col_diff+3)//2+20
+
+    return int(start_idx + move_type_index*28)
+
+
+def a0_to_move(action: int) -> str:
+    L_move = {12: (-1,-1), 13: (1,-1), 14:(-1,1), 15:(1,1),
+              16: (-2,-1), 17: (-2,1), 18:(2,-1), 19:(2,1),
+              20: (-1,-2), 21: (1,-2), 22:(-1,2), 23:(1,2)}
+    start_idx = action % 28
+    move_type_index = action // 28
+    start_row = start_idx // 7
+    start_col = start_idx % 7
+    start_square = chr(start_col + 97) + str(start_row + 1)
+
+    if move_type_index < 3:
+        end_square = chr(start_col+97) + str(start_row -3 + move_type_index + 1)
+    elif move_type_index < 6:
+        end_square = chr(start_col+97) + str(start_row + move_type_index - 1)
+    elif move_type_index < 9:
+        end_square = chr(start_col+97 + move_type_index - 9) + str(start_row + 1)
+    elif move_type_index < 12:
+        end_square = chr(start_col+97 + move_type_index - 8) + str(start_row + 1)
+    else:
+        end_square = chr(start_col+97 + L_move[move_type_index][1]) + str(start_row + L_move[move_type_index][0] + 1)
+    
+    return start_square + end_square
+
+
+def moves_to_a0(moves: list[str]) -> list[int]:
+    return [move_to_a0(move) for move in moves]
+
+
+def parallel_valid_policy(policies: np.ndarray, envs: list[Kita]) -> np.ndarray:
+    valid_moves = [list(env.get_valid_moves()) for env in envs]
+    encoded_valid_moves = [moves_to_a0(moves) for moves in valid_moves]
+    mask = np.zeros(shape=(len(envs), 672))
+    for i, moves in enumerate(encoded_valid_moves):
+        mask[i, moves] = 1
+    valid_policy = mask * policies
+    row_sums = np.sum(valid_policy, axis=1, keepdims=True, dtype=np.float32)
+    valid_policy /=  np.where(row_sums != 0, row_sums, 1)
+    return valid_policy
+
+def valid_policy(policy: np.ndarray, env: Kita) -> np.ndarray:
+    valid_moves = list(env.get_valid_moves())
+    encoded_valid_moves = moves_to_a0(valid_moves)
+    mask = np.zeros(672)
+    mask[encoded_valid_moves] = 1
+    valid_policy = mask * policy
+    valid_policy /= (np.sum(valid_policy) if np.sum(valid_policy) != 0 else 1)
+    return valid_policy
+
+
+def prepare_input(env: Kita) -> torch.Tensor:
+    matrix = board_to_matrix(env)
+    X_tensor = torch.tensor(matrix, dtype=torch.float32)
+    return X_tensor
+
+
+
+
+
+
+
